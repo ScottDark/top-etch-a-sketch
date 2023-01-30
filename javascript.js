2e4ef233d7ee66665b787dfd33753646bbfa3310
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-30 18:27:03 -0500

    Add ability to create 16 divs on container

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
-/* Returns: Divs */
-function createDefaultDivGrid() {
-  const container = document.querySelector(".container");
-  const div = document.createElement("div");
-
-  container.appendChild(div);
-}
-
 window.onload = createDefaultDivGrid();
+
+/* Creates 16x16 divs */
+function createDefaultDivGrid() {
+  //   let numberOfChildDivs = container.children.length + 1;
+
+  for (let i = 1; i <= 16; i++) {
+    const container = document.querySelector(".container");
+    const div = document.createElement("div");
+    const createDiv = container.appendChild(div);
+    let addClassToDiv = createDiv.classList.add(`div${i}`);
+    container.addClassToDiv;
+  }
+}
