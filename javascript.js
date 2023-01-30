bdb81876a9e61ea4211aaf7c038821c3c3fa0a3e
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-30 17:41:47 -0500

    onload for default div 16x16

--- javascript.js
+++ javascript.js
@@ -1,2 +1,9 @@
 /* Returns: Divs */
-function createDefaultDivGrid() {}
+function createDefaultDivGrid() {
+  const container = document.querySelector(".container");
+  const div = document.createElement("div");
+
+  container.appendChild(div);
+}
+
+window.onload = createDefaultDivGrid();
