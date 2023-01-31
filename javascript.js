f7e43e3188346a2083055122b2b6415762536744
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-31 13:24:02 -0500

    Responsive 16x16 default grid setup

--- javascript.js
+++ javascript.js
@@ -3,22 +3,21 @@ window.onload = createDefaultDivGrid();
 /* Creates 16x16 divs */
 function createDefaultDivGrid() {
   // Creates 16 divs sharing same class name and unique iterable class name
-  for (let i = 1; i <= 16; i++) {
-    const container = document.querySelector(".container");
+  for (let i = 1; i <= 256; i++) {
+    const gridContainer = document.querySelector(".gridContainer");
     const div = document.createElement("div");
-    const createDiv = container.appendChild(div);
+    const createDiv = gridContainer.appendChild(div);
     let addDefaultClassToDiv = createDiv.classList.add(`divSquare`);
     let addClassToDiv = createDiv.classList.add(`div${i}`);
-    container.addClassToDiv;
-    container.addDefaultClassToDiv;
+    gridContainer.addClassToDiv;
+    gridContainer.addDefaultClassToDiv;
   }
   // Insert css rule to stylesheet
   const stylesheet = window.document.styleSheets[0];
   stylesheet.insertRule(
     `.divSquare { 
-        border: solid black 3px; 
-        width: 20vw;
-        flex-grow: 1;}`,
+      background-color: rgba(255, 255, 255, 0.8);
+      text-align: center;}`,
     stylesheet.cssRules.length
   );
 }
